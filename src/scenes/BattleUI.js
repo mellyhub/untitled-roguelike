400071a348b841aa75e84c7a6916512e680221de
lusj3822 <ludvigsjoberg160@gmail.com>
2025-04-06 02:08:42 +0200

    fixed a bug with duplicate rendering of player stats and score/turn

--- src/scenes/BattleUI.js
+++ src/scenes/BattleUI.js
@@ -1,12 +1,12 @@
 class BattleUI {
     constructor(scene) {
         this.scene = scene;
-        this.renderedElements = [];
         this.menuItems = [];
         this.currentMenu = null;
         this.currentSelection = { x: 0, y: 0 };
         this.currentMenuType = 'main';
         this.spellMenuBackground = null;
+        this.statsContainer = null;
     }
 
     calculateHealthBarSize(maxUnitHealth, currentUnitHealth) {
@@ -19,7 +19,7 @@ class BattleUI {
 
     displayHealthBarBorder(x, y, width, height) {
         const borderSize = 4;
-        this.scene.add.rectangle(
+        return this.scene.add.rectangle(
             x - borderSize / 2,
             y - borderSize / 2,
             width + borderSize,
@@ -36,34 +36,36 @@ class BattleUI {
             BLACK: Phaser.Display.Color.GetColor32(0, 0, 0, 255)
         };
 
-        // cleanup old renders
-        this.renderedElements.forEach(render => render.destroy());
+        if (this.statsContainer) {
+            this.statsContainer.destroy(true);
+        }
+        this.statsContainer = this.scene.add.container(0, 0);
 
         // score
-        this.renderedElements.push(this.scene.add.text(0, 0, `Score: ${player.score}`, { fontSize: '32px', fill: '#fff' }));
+        this.statsContainer.add(this.scene.add.text(0, 0, `Score: ${player.score}`, { fontSize: '32px', fill: '#fff' }));
 
         // turn counter
-        this.renderedElements.push(this.scene.add.text(0, 50, `Turn: ${turnCounter}`, { fontSize: '32px', fill: '#fff' }));
+        this.statsContainer.add(this.scene.add.text(0, 50, `Turn: ${turnCounter}`, { fontSize: '32px', fill: '#fff' }));
 
         // players health bar
         const playerHealthBarSize = this.calculateHealthBarSize(playerStartHP, player.health);
-        this.displayHealthBarBorder(400, 200, 500, 50);
-        this.renderedElements.push(this.scene.add.rectangle(400, 200, playerHealthBarSize.width, playerHealthBarSize.height, COLOR_CODES.GREEN).setOrigin(0));
-        this.renderedElements.push(this.scene.add.text(400, 100, `Class: ${player.class.name}`, { fontSize: '52px' }));
-        this.renderedElements.push(this.scene.add.text(400, 150, `Level: ${player.level}`, { fontSize: '52px' }));
-        this.renderedElements.push(this.scene.add.text(400, 200, `${player.name}: ${Math.max(0, player.health)} HP`, { fontSize: '52px' }));
+        this.statsContainer.add(this.displayHealthBarBorder(400, 200, 500, 50));
+        this.statsContainer.add(this.scene.add.rectangle(400, 200, playerHealthBarSize.width, playerHealthBarSize.height, COLOR_CODES.GREEN).setOrigin(0));
+        this.statsContainer.add(this.scene.add.text(400, 100, `Class: ${player.class.name}`, { fontSize: '52px' }));
+        this.statsContainer.add(this.scene.add.text(400, 150, `Level: ${player.level}`, { fontSize: '52px' }));
+        this.statsContainer.add(this.scene.add.text(400, 200, `${player.name}: ${Math.max(0, player.health)} HP`, { fontSize: '52px' }));
 
         // enemy health bar
         const enemyHealthBarSize = this.calculateHealthBarSize(enemyStartHP, enemy.health);
-        this.displayHealthBarBorder(1220, 200, 500, 50);
-        this.renderedElements.push(this.scene.add.rectangle(1220, 200, enemyHealthBarSize.width, enemyHealthBarSize.height, COLOR_CODES.RED).setOrigin(0));
-        this.renderedElements.push(this.scene.add.text(1220, 200, `${enemy.name}: ${Math.max(0, enemy.health)} HP`, { fontSize: '52px' }));
+        this.statsContainer.add(this.displayHealthBarBorder(1220, 200, 500, 50));
+        this.statsContainer.add(this.scene.add.rectangle(1220, 200, enemyHealthBarSize.width, enemyHealthBarSize.height, COLOR_CODES.RED).setOrigin(0));
+        this.statsContainer.add(this.scene.add.text(1220, 200, `${enemy.name}: ${Math.max(0, enemy.health)} HP`, { fontSize: '52px' }));
 
         // player stats
-        this.renderedElements.push(this.scene.add.text(1100, 820, `Strength: ${player.stats.strength}`, { fontSize: '24px', fill: '#fff' }).setOrigin(0.5));
-        this.renderedElements.push(this.scene.add.text(1100, 870, `Agility: ${player.stats.agility}`, { fontSize: '24px', fill: '#fff' }).setOrigin(0.5));
-        this.renderedElements.push(this.scene.add.text(1100, 920, `Intelligence: ${player.stats.intelligence}`, { fontSize: '24px', fill: '#fff' }).setOrigin(0.5));
-        this.renderedElements.push(this.scene.add.rectangle(1250, 800, 400, 200, Phaser.Display.Color.GetColor32(79, 52, 41, 255)).setOrigin(0));
+        this.statsContainer.add(this.scene.add.text(1100, 820, `Strength: ${player.stats.strength}`, { fontSize: '24px', fill: '#fff' }).setOrigin(0.5));
+        this.statsContainer.add(this.scene.add.text(1100, 870, `Agility: ${player.stats.agility}`, { fontSize: '24px', fill: '#fff' }).setOrigin(0.5));
+        this.statsContainer.add(this.scene.add.text(1100, 920, `Intelligence: ${player.stats.intelligence}`, { fontSize: '24px', fill: '#fff' }).setOrigin(0.5));
+        this.statsContainer.add(this.scene.add.rectangle(1250, 800, 400, 200, Phaser.Display.Color.GetColor32(79, 52, 41, 255)).setOrigin(0));
     }
 
     renderMenu(menu) {
@@ -72,7 +74,6 @@ class BattleUI {
             this.menuItems.forEach(item => item.destroy());
         }
 
-        this.renderedElements = [];
         this.currentMenu = menu;
 
         // render menu items
