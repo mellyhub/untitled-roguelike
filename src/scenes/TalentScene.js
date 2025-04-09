89cb1afd17c5606e606650c68e582dd280077ae6
HectorLundman <hectorlundman@gmail.com>
2025-04-09 11:54:16 +0200

    TalentScene 2d array moments

--- src/scenes/TalentScene.js
+++ src/scenes/TalentScene.js
@@ -21,14 +21,23 @@ class TalentScene extends Phaser.Scene {
     });
 
     // behöver hitta en mer dynamisk lösning på hur vi interagerar med spelare objektet
-  
-    this.attributes = [
-      { name: 'Talent1', value: 0 },
-      { name: 'Talent2', value: 0 },
-      { name: 'Talent3', value: 0 },
-    ];
 
-    this.currentSelection = 0;
+    this.talents = [];
+    this.talentRows = 5;
+    this.talentColumns = 5;
+    for (let i = 0; i < this.talentRows; i++) {
+      for (let j = 0; j < this.talentColumns; j++) {
+        this.talents.push({
+          x: i,
+          y: j,
+          name: `T ${i * this.talentColumns + j + 1}`,
+          value: 0,
+        });
+      }
+    }
+    console.log(this.talents);
+
+    this.currentSelection = { x: 0, y: 0 };
 
     this.renderUI();
   }
@@ -47,51 +56,67 @@ class TalentScene extends Phaser.Scene {
     }).setOrigin(0.5);
     this.uiElements.push(talentPointsText);
 
-    // visa attribut
-    this.attributes.forEach((attribute, index) => {
-      const yPosition = 300 + index * 100;
-      const text = this.add.text(960, yPosition, `${attribute.name}: ${attribute.value}`, {
-        fontSize: '48px',
-        fill: '#fff',
-      }).setOrigin(0.5);
+    var talentText;
 
-      // highlightar alternativet med röd färg
-      if (index === this.currentSelection) {
-        text.setColor('#ff0000');
+    for (let i = 0; i < this.talentRows; i++) {
+      for (let j = 0; j < this.talentColumns; j++) {
+        talentText = this.add.text(100 + j * 200, 300 + i * 100, `${this.talents[i * this.talentColumns + j].name}: ${this.talents[i * this.talentColumns + j].value}`, {
+          fontSize: '32px',
+          fill: '#fff',
+        }).setOrigin(0.5);
+
+        this.uiElements.push(talentText);
       }
+    }
 
-      this.uiElements.push(text);
-    });
+    if (this.talents.x === this.currentSelection.x && this.talents.y === this.currentSelection.y) {
+      talentText.setColor('#ff0000');
+    }
+    this.uiElements.push(talentText);
   }
 
   update() {
     // navigerar menyn med pilarna
-    if (Phaser.Input.Keyboard.JustDown(this.cursors.up) || Phaser.Input.Keyboard.JustDown(this.cursors.left)) {
-      this.changeSelection(-1);
+    if (Phaser.Input.Keyboard.JustDown(this.cursors.up)) {
+      this.changeSelection(0, -1);
+      console.log(this.currentSelection);
     }
-    else if (Phaser.Input.Keyboard.JustDown(this.cursors.down) || Phaser.Input.Keyboard.JustDown(this.cursors.right)) {
-      this.changeSelection(1);
+    else if (Phaser.Input.Keyboard.JustDown(this.cursors.down)) {
+      this.changeSelection(0, 1);
+      console.log(this.currentSelection);
+      console.log(this.talents[0][0].y);
+    }
+    else if (Phaser.Input.Keyboard.JustDown(this.cursors.left)) {
+      this.changeSelection(-1, 0);
+      console.log(this.currentSelection);
+    }
+    else if (Phaser.Input.Keyboard.JustDown(this.cursors.right)) {
+      this.changeSelection(1, 0);
+      console.log(this.currentSelection);
     }
 
     // allokerar poäng med enter
     if (Phaser.Input.Keyboard.JustDown(this.enterKey)) {
-      this.allocatePoint();
+      this.allocatePoint(this.currentSelection);
     }
   }
 
-  changeSelection(direction) {
+  changeSelection(xDirection, yDirection) {
     // uppdaterar vald selection
-    this.currentSelection = (this.currentSelection + direction + this.attributes.length) % this.attributes.length;
+    this.currentSelection = {
+      x: (this.currentSelection.x + xDirection + this.talentColumns) % this.talentColumns,
+      y: (this.currentSelection.y + yDirection + this.talentRows) % this.talentRows,
+    };
 
     // uppdaterar ui för att visa nya valet
     this.renderUI();
   }
 
-  allocatePoint() {
+  allocatePoint(currentSelection) {
     // kolla ifall det finns tillgängliga poäng
     if (this.player.talentPoints > 0) {
       // ökar värde på valt attribut
-      this.attributes[this.currentSelection].value += 1;
+      this.talents[currentSelection.x][currentSelection.y].value += 1;
 
       // spenderar en talent point
       this.player.talentPoints -= 1;
