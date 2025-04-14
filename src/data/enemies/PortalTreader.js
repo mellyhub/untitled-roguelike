c1366a8254de0105bdb45812333eaa6cb3b82e45
HectorLundman <hectorlundman@gmail.com>
2025-04-14 21:49:50 +0200

    Added sprites and class for new enemy
    New enemy: Portal Treader. Not yet fully implemented but there is both an idle sprite sheet, as well as an attack sprite sheet for this one :)

---
+++ src/data/enemies/PortalTreader.js
@@ -0,0 +1,25 @@
+export class PortalTreader {
+    name = "Portal Treader";
+    battleCry = null;
+    animationKey = "portal-treader-anim";
+    animationSheetName = "portal treader idle";
+    animationFrameRate = 6;
+    imageScale = 1;
+    imageXPos = 1440;
+    imageYPos = 540;
+
+    health;
+    strength;
+    energy = 100;
+    maxEnergy = 100;
+    spells = [];
+
+    constructor(weight) {
+        this.health = 190 + weight * 10;
+        this.strength = 100 + weight * 10;
+    }
+
+    attack(target) {
+        target.health -= 50;
+    }
+}
