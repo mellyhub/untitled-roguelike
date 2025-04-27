d8ad21a57fcd8de796be5d3d89b7292071634e8b
lusj3822 <ludvigsjoberg160@gmail.com>
2025-04-27 20:01:20 +0200

    fixed duplicate adding of animations

--- src/data/Animations.js
+++ src/data/Animations.js
@@ -14,6 +14,8 @@ export class Animations {
     attackAnimation;
     castAnimation;
 
+    initialized;
+
     constructor(unitName, idleAssetKey, attackAssetKey, castAssetKey, properties) {
         this.unitName = unitName;
         this.idleAnimationKey = `${unitName} idle`;
@@ -26,6 +28,7 @@ export class Animations {
         this.castAnimationSheetKey = castAssetKey;
 
         this.properties = properties;
+        this.initialized = false;
     }
 
     playIdleAnimation() {
@@ -79,15 +82,18 @@ export class Animations {
     }    
 
     createAnimations(scene) {
-        this.createIdleAnimation(scene);
-        this.createAttackAnimation(scene);
-        this.createCastAnimation(scene);
+        if (!this.initialized) {
+            this.createIdleAnimation(scene);
+            this.createAttackAnimation(scene);
+            this.createCastAnimation(scene);
+        }
         this.idleAnimation = scene.add.sprite(this.properties.idleAnimPos.x, this.properties.idleAnimPos.y, this.idleAnimationSheetKey).setScale(1.2);
         this.attackAnimation = scene.add.sprite(this.properties.attackAnimPos.x, this.properties.attackAnimPos.y, this.attackAnimationSheetKey).setScale(1.2);
         this.castAnimation = scene.add.sprite(this.properties.castAnimPos.x, this.properties.castAnimPos.y, this.castAnimationSheetKey).setScale(1.2);
         this.idleAnimation.setVisible(true);
         this.attackAnimation.setVisible(false);
         this.castAnimation.setVisible(false);
+        this.initialized = true;
     }
 
     createIdleAnimation(scene) {
